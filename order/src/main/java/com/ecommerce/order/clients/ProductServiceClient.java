1d76983dbd8ce828efa8d1a9b29af631b9a2c8da
Ikedinobi <Ikdinobi@gmail.com>
2025-10-31 13:42:15 +0100

    Feature: Ecommerce Microservice Service Discovery using Eureka

---
+++ order/src/main/java/com/ecommerce/order/clients/ProductServiceClient.java
@@ -0,0 +1,9 @@
+package com.ecommerce.order.clients;
+
+
+//@HttpExchange
+public interface ProductServiceClient {
+
+//    @GetExchange("/api/products/{id}")
+//    ProductResponse getProductDetails(@PathVariable String id);
+}
---
+++ order/src/main/java/com/ecommerce/order/clients/UserServiceClient.java
@@ -0,0 +1,9 @@
+package com.ecommerce.order.clients;
+
+
+//@HttpExchange
+public interface UserServiceClient {
+
+//    @GetExchange("/api/users/{id}")
+//    UserResponse getUserDetails(@PathVariable String id);
+}
