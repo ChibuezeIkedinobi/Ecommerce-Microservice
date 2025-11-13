014677d641690b6cb9072bd9b003bf61e35fa3e3
Ikedinobi <Ikdinobi@gmail.com>
2025-11-13 01:16:15 +0100

    Zipkin configuration I

--- order/src/main/java/com/ecommerce/order/clients/RestClientConfig.java
+++ order/src/main/java/com/ecommerce/order/clients/RestClientConfig.java
@@ -1,7 +1,8 @@
 package com.ecommerce.order.clients;
 
 import io.micrometer.observation.ObservationRegistry;
-import io.micrometer.observation.transport.Propagator;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
@@ -12,39 +13,38 @@ import org.springframework.web.client.RestClient;
 @Configuration
 public class RestClientConfig {
 
-//    @Autowired(required = false)
-//    private ObservationRegistry observationRegistry;
-//
-//    @Autowired(required = false)
-////    private Tracer tracer;
-//
-//    @Autowired(required = false)
-//    private Propagator propagator;
+    @Autowired(required = false)
+    private ObservationRegistry observationRegistry;
+
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private Propagator propagator;
 
     @Bean
     @LoadBalanced
     public RestClient.Builder restClientBuilder() {
         RestClient.Builder builder = RestClient.builder();
 
-//        if (observationRegistry != null){
-//            builder.requestInterceptor(createTracingInterceptor());
-//        }
+        if (observationRegistry != null){
+            builder.requestInterceptor(createTracingInterceptor());
+        }
 
         return builder;
     }
 
-//    private ClientHttpRequestInterceptor createTracingInterceptor() {
-//        return ((request, body, execution) -> {
-//            if (tracer != null && propagator != null
-//                    && tracer.currentSpan() != null) {
-//                propagator.inject(tracer.currentTraceContext().context(),
-//                        request.getHeaders(),
-//                        (carrier, key, value) -> carrier.add(key, value));
-//            }
-//            return execution.execute(request, body);
-//        }
-//        );
-//    }
+    private ClientHttpRequestInterceptor createTracingInterceptor() {
+        return ((request, body, execution) -> {
+            if (tracer != null && propagator != null && tracer.currentSpan() != null) {
+                propagator.inject(
+                        tracer.currentTraceContext().context(),
+                        request.getHeaders(),
+                        (carrier, key, value) -> carrier.add(key, value));
+            }
+            return execution.execute(request, body);
+        });
+    }
 
 
 }
